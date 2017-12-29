f9934539f513ca3305915cebcbf40f58a603afb4
MNavas <mnavas@delicias.dia.fi.upm.es>
2017-12-29 19:25:28 +0100

    test outfit

--- src/main/java/oeg/legalwhen/lawordate/TestLawORDate.java
+++ src/main/java/oeg/legalwhen/lawordate/TestLawORDate.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author mnavas
  */
-public class getText extends HttpServlet {
+public class TestLawORDate extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
